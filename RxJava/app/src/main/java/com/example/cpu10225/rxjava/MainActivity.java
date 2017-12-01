c262978d0867c9a97103c8d338fd4573f3c3da1e
HoangkimTuan <hoangkimtuan1996@gmail.com>
2017-12-01 11:23:29 +0700

    RxJava: Demo Transforming Observables

--- RxJava/app/src/main/java/com/example/cpu10225/rxjava/MainActivity.java
+++ RxJava/app/src/main/java/com/example/cpu10225/rxjava/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.cpu10225.rxjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.functions.Function;
+import io.reactivex.observables.GroupedObservable;
 
 public class MainActivity extends AppCompatActivity {
     String movie;
@@ -30,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         movieObservable.subscribe(System.out::println);
 
         // Interval
-        Observable.interval(1, TimeUnit.SECONDS).subscribe(System.out::println);
+        // Observable.interval(1, TimeUnit.SECONDS).subscribe(System.out::println);
 
         // Create
         Observable.create( subscriber-> {
@@ -39,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
             subscriber.onNext(3);
             subscriber.onComplete();
         }).subscribe(System.out::println);
-
 
         // Range
         Observable.range(0, 10)
@@ -53,14 +55,53 @@ public class MainActivity extends AppCompatActivity {
         Observable.never().subscribe(System.out::println);
 
         // Throw
-        Observable.error(new RuntimeException()).subscribe(System.out::println);
+        // Observable.error(new RuntimeException()).subscribe(System.out::println);
 
         // Repeat
-        Observable.just(integers).repeatWhen(new Function<Observable<Object>, ObservableSource<?>>() {
+        AtomicInteger c = new AtomicInteger();
+        Observable.just(1)
+                .repeatWhen(o -> o.takeWhile((v -> c.getAndIncrement() == 0)))
+                .subscribe(System.out::println, Throwable::printStackTrace, () -> System.out.println("Done"));
+
+        // Start
+        Observable.fromCallable(new Callable<String>() {
             @Override
-            public ObservableSource<?> apply(Observable<Object> objectObservable) throws Exception {
-                return null;
+            public String call() throws Exception {
+                return "Hello";
             }
         });
+
+        // Timer
+        Observable.timer(1, TimeUnit.SECONDS).subscribe(System.out::println);
+
+        // Buffer
+        Observable.range(0, 10)
+                .buffer(3, 4)
+                .subscribe(System.out::println);
+
+        // FlatMap
+        Observable.just(integers)
+                .flatMap(ints -> Observable.fromArray(ints))
+                .subscribe(System.out::println);
+
+        // GroupBy
+        Observable.range(0, 10)
+                .groupBy(n -> n % 2 == 0)
+                .subscribe((group) -> {((GroupedObservable)group).subscribe((x) -> System.out.println(((GroupedObservable)group).getKey() + ": " + x));});
+
+        // Map
+        Observable.fromArray(integers)
+                .map(i -> "STT: " + i)
+                .subscribe(System.out::println);
+
+        // Scan ???
+        Observable.range(1, 10)
+                .scan((integer, integer2) -> integer2 + integer)
+                .subscribe(System.out::println);
+
+        // Window ???
+        Observable.range(0, 5)
+                .window(3, 1)
+                .subscribe(System.out::println);
     }
 }
